339936b475166ef05ba9b83bd48b67fdd7610c91
Ritika Gupta <guptaritika782@gmail.com>
2021-04-28 12:25:44 +0530

    Installed material-ui and Added Header-left portion

--- facebook_clone-app/src/components/Header/Header.js
+++ facebook_clone-app/src/components/Header/Header.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 import './Header.css';
 
+import SearchIcon from '@material-ui/icons/Search';
+
 const Header = () => {
   return (
     <div className="header">
       <div className="header__left">
-        LEFT
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="FacebookLogo" />
+        
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
       </div>
+
       <div className="header__middle">
         MIDDLE
       </div>
