e0aaa4a3327308751431d6aaa9de0c50cbb7cb0f
Ritika Gupta <guptaritika782@gmail.com>
2021-04-28 12:36:46 +0530

    Added Header-middle portion

--- facebook_clone-app/src/components/Header/Header.js
+++ facebook_clone-app/src/components/Header/Header.js
@@ -4,6 +4,12 @@ import './Header.css';
 
 import SearchIcon from '@material-ui/icons/Search';
 
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+
 const Header = () => {
   return (
     <div className="header">
@@ -17,7 +23,21 @@ const Header = () => {
       </div>
 
       <div className="header__middle">
-        MIDDLE
+        <div className="header__option header__option--active">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
       </div>
       <div className="header__right">
         RIGHT
